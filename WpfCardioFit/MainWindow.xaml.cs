52634bd530e368c191f3c9ab620b6f8ef47a425c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-05-12 12:42:11 +0200

    aggiunta wpf

---
+++ WpfCardioFit/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AllenamentoEfficace;
+
+namespace WpfCardioFit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcola_Click(object sender, RoutedEventArgs e)
+        {
+            string frequenza;
+            frequenza = Gestione.btmMaxMin(int.Parse(txt_letturaeta.Text));
+            string[] freqMaxMin = new string[2];
+            freqMaxMin = frequenza.Split(" ");
+            lbl_frequenzaMax.Content = freqMaxMin[0] + " btm";
+            lbl_FrequenzaMin.Content = freqMaxMin[1] + " btm";
+            int valore = int.Parse(txt_btmDaCercare.Text);
+            bool Risultato = Gestione.BattitiRisposo(valore);
+            if (Risultato)
+                lbl_RIspostaBtmMedio.Content = "il valore inserito è presente nel file";
+            else
+                lbl_RIspostaBtmMedio.Content= "il valore  inserito non è presente nel file";
+
+
+
+        }
+    }
+}
